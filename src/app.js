13f4dd704ba6147f7aa0dc071ada0c832bb9f2c6
efrat-code <efrathalle17@gmail.com>
2024-10-07 10:32:22 +0300

    biuld the basice server and files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("object")
+const exp = require('express')
+const dotenv = require('dotenv')
+
+const app = exp()
+dotenv.config()
+
+app.use(exp.json())
+app.use('auth',require('./routes/authRouter'))
+app.use('user',require('./routes/userRouter'))
+app.use('green_eye',require('./routes/green_eye'))
+
+const port = process.env.PORT 
+app.listen(port ,() => console.log(`server run on port ${port}`))
+
